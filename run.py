564ff34a67ebee331dd9d3a266908e25b3b85813
jinwooyoon <say_stop@naver.com>
2024-01-10 12:53:23 +0900

    update

--- run.py
+++
@@ -1,6 +0,0 @@
-from edi_package.device import DeviceTransform,DeviceTranslate
-from edi_package.suga import SugaTransform,SugaTranslate
-from edi_package.drug import DrugTransform,DrugTranslate
-from edi_package.sql_process import DBConnect
-
-connect = DBConnect(host="",port="", user="", password="",database="")
